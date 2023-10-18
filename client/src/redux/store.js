ebd56da8e225fe64be58d0b1d8f9cf489e6ca021
Kapil Paliwal <paliwalkapil2004@gmail.com>
2023-10-18 23:51:32 +0530

    add redux toolkit

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,8 @@
 import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit';
+import userSlice from './user/userSlice';
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {user: userSlice},
     middleware: (getDefaultMiddleware)=> getDefaultMiddleware({
         serializableCheck: false,
     }),
