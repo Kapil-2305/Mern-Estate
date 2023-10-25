08e012de5243ce08a4a836290a824049a6ed30fd
Kapil Paliwal <paliwalkapil2004@gmail.com>
2023-10-26 05:15:29 +0530

    create user update api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,37 @@
-export const test = (req, res)=>{
+import bcrypt from 'bcrypt';
+import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
+
+export const test = (req, res, next)=>{
     res.json({
         message: "hello world!",
     });
+};
+
+export const updateUser = async (req, res, next)=>{
+    if(req.user.id !== req.params.id){
+        return next(errorHandler(401, 'You can only update your own account!'));
+    }
+
+    try{
+        if(req.body.password){
+            req.body.password = bcrypt.hashSync(req.body.password, 10);
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar,
+            }
+        }, {new: true});
+
+        const {password, ...others} = updatedUser._doc;
+
+        res.status(200).json(others);
+    }
+    catch(err){
+        return next(errorHandler(500, 'Internal Server Error'));
+    }
 };
