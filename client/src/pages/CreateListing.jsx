0f43f3dc2786d7c9452dbfc0dce6c569e162bc94
Kapil Paliwal <paliwalkapil2004@gmail.com>
2023-11-04 23:47:17 +0530

    complete create listing page UI

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -4,7 +4,7 @@ const CreateListing = () => {
   return (
     <main className='p-3 max-w-4xl mx-auto'>
         <h1 className='text-3xl font-semibold text-center my-7'>Create a Listing</h1>
-        <form className='flex flex-col sm:flex-row'>
+        <form className='flex flex-col sm:flex-row gap-4'>
             <div className='flex flex-col gap-4 flex-1'>
                 <input type='text' placeholder='Name' className='border p-3 rounded-lg' id='name' maxLength='62' minLength='10' required/>
                 <textarea placeholder='Description' className='border p-3 rounded-lg' id='description' required/>
@@ -33,7 +33,7 @@ const CreateListing = () => {
                     </div>
                 </div>
 
-                <div className=''>
+                <div className='flex flex-wrap gap-6'>
                     <div className='flex items-center gap-2'>
                         <input type='number' id='bedrooms' min={1} max={10} required className='p-3 border border-gray-300 rounded-lg'/>
                         <p>Beds</p>
@@ -57,6 +57,21 @@ const CreateListing = () => {
                         </div>
                     </div>
                 </div>
+            </div>
+
+            <div className='flex flex-col flex-1 gap-4'>
+                <p className='font-semibold'>
+                    Images:
+                    <span className='font-normal text-gray-700 ml-2'>
+                        The first image will be the cover (max 6)
+                    </span>
+                </p>
+                <div className='flex gap-4'>
+                    <input className='p-3 border border-gray-300 rounded w-full' type='file' id='images' accept='image/*' multiple />
+                    <button className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>Upload</button>
+                </div>
+
+                <button className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Create Listing</button>
             </div>
         </form>
     </main>
